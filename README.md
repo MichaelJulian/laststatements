10a095e94f6bfd541050e371caf618b4b8e06108
MichaelJulian <mikeyjmikeyj@gmail.com>
2016-07-25 17:07:16 -0700

    organize data and readme

---
+++ README.md
@@ -0,0 +1,25 @@
+# Death Row Inmate Visualizations
+
+This repo is for a 2-hour sprint by Jamen Long, Tim Bearden, and Michael Julian.
+
+We scraped last statements from death row inmates, provided by the State of Texas:
+
+http://www.tdcj.state.tx.us/death_row/dr_executed_offenders.html
+
+
+## Deaths Across Time
+
+![Deaths by Year](plots/Deaths%20Per%20Year.png)
+
+![By Month Date](plots/Deaths%20by%20Month%20Date.png)
+
+
+## Deaths by Background
+
+![By Ethnicity](plots/Deaths%20by%20Race.png)
+
+![Deaths by County](plots/Deaths%20by%20County.png)
+
+## Most Common Trigrams
+
+![Last Words](plots/ngram_7.png)
---
+++ code/inmateVIS.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from collections import Counter
+from datetime import datetime
+import numpy as np
+import seaborn as sb
+
+inmates = pd.read_csv("Inmate_info.csv")
+plt.hist(inmates['Age'])
+plt.title("Inmate Age")
+plt.show()
+
+inmates['Date'] = pd.to_datetime(inmates['Date'])
+inmates['MonthYear'] = inmates['Date'].apply(lambda x:x.date().strftime('%m %Y'))
+inmates['Month'] = inmates['Date'].apply(lambda x:x.date().strftime('%m'))
+inmates['Day'] = inmates['Date'].apply(lambda x:x.date().strftime('%d'))
+inmates['Year'] = inmates['Date'].apply(lambda x:x.date().strftime('%Y'))
+
+plt.hist(list(map(int,inmates['Year'])))
+plt.title("Deaths Per Year")
+plt.savefig("Deaths Per Year")
+plt.show()
+
+plt.hist(list(map(int, inmates['Month'])))
+plt.title("Deaths by Month")
+plt.savefig("Deaths by Month")
+plt.show()
+
+plt.hist(list(map(int, inmates['Day'])))
+plt.title("Deaths by Month Date")
+plt.savefig("Deaths by Month Date")
+plt.show()
+
+labels, values = zip(*Counter(inmates['Race']).items())
+indexes = np.arange(len(labels))
+plt.bar(indexes, values, 1)
+plt.xticks(indexes+1*.5, labels)
+plt.title("Deaths by Race")
+plt.savefig("Deaths Per Year")
+plt.show()
+#
+# labels, values = zip(*Counter(inmates['County']).most_common(15).items())
+# indexes = np.arange(len(labels))
+# plt.bar(indexes, values, 1)
+# plt.xticks(indexes+1*.5, labels)
+# plt.title("Deaths by County")
+# plt.savefig("Deaths by County")
+# plt.show()
+
+c = Counter(inmates['County'])
+most_common_counties = c.most_common(15)
+labels = [mcc[0] for mcc in most_common_counties]
+values = [mcc[1] for mcc in most_common_counties]
+plt.bar(range(len(labels)), values)
+plt.xticks(range(len(labels)), labels)
+plt.title("Deaths by County")
+plt.savefig("Deaths by County")
+plt.show()
+
+# violin_data = inmates.ix[:, 6:]
+# daterace = violin_data.ix[:, (0,2)]
+# sb.violinplot(x = "Race", y = "Age", data = daterace)
+# plt.show()
